c3b136e572df61bb02f9e4fba2af0fd83265b402
mateoroldos <mateoroldos19@gmail.com>
2022-09-26 15:19:14 -0300

    feat: Update @emerald-dao/component-library and add Tagline component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,38 @@
 <script type="ts">
-  import dappInfo from "$lib/config/config";
-  import { Section, Container, Column } from "@mateoroldos/svelte.bones";
-  import { transactionInProgress } from '$stores/FlowStore'
+	import dappInfo from '$lib/config/config';
+	import { Section, Container, Column, Row } from '@mateoroldos/svelte.bones';
+	import { transactionInProgress } from '$stores/FlowStore';
+	import { Button, Tagline } from '@emerald-dao/component-library';
 </script>
 
 <Section>
-  {#if $transactionInProgress}
-    <div>aaa</div>
-  {/if}
-  <Container>
-    <Column>
-      <h1>Welcome to <span>{dappInfo.title}</span></h1>
-      <p>{dappInfo.description}</p>
-      <p>Crafted by {dappInfo.author}</p>
-    </Column>
-  </Container>
+	<Container>
+		<Column>
+			<h1>Create a token.<br /> Launch a DAO</h1>
+			<p>
+				Toucans is the first fungible token creation and management platform on the Flow blockchain
+				that makes it easy for internet communities unlock the power of human-centric coordination.
+			</p>
+			<Row gap="small">
+				<Button>Launch a Token</Button>
+				<Button type="ghost">Browse Projects</Button>
+			</Row>
+		</Column>
+	</Container>
+</Section>
+
+<Section>
+	<Container>
+		<Column>
+			<Tagline>Why Start a DAO?</Tagline>
+			<h1>Tokens for any Community</h1>
+		</Column>
+	</Container>
 </Section>
 
 <style type="scss">
-  h1, p {
-    text-align: center;
-  }
-
-  span {
-    color: var(--clr-primary-main)
-  }
+	h1,
+	p {
+		text-align: center;
+	}
 </style>
