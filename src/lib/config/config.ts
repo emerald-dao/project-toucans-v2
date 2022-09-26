c878139e104b26f8b4d88693a44077452e52a220
mateoroldos <mateoroldos19@gmail.com>
2022-09-26 14:45:51 -0300

    feat: Initial commit

---
+++ src/lib/config/config.ts
@@ -0,0 +1,9 @@
+const dappInfo = {
+	title: 'Toucans',
+	description: 'No-code tools for DAOs on the Flow Blockchain',
+	url: 'pending.com',
+	author: 'Emerald City DAO',
+	icon: 'https://i.imgur.com/KSL4gq0.png'
+};
+
+export default dappInfo;
