6c2bbfc42a66f402f92706353d655db40841c4af
mateoroldos <mateoroldos19@gmail.com>
2022-10-13 17:37:39 -0300

    feat: Add chart.js

---
+++ src/lib/components/charts/LineChart.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { Line } from 'svelte-chartjs';
+
+	export let title;
+	export let chartData;
+	export let labels;
+
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		LineElement,
+		LinearScale,
+		PointElement,
+		CategoryScale
+	} from 'chart.js';
+
+	ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale);
+
+	const data = {
+		labels,
+		datasets: [
+			{
+				label: title,
+				fill: true,
+				lineTension: 0.3,
+				backgroundColor: 'green',
+				borderColor: 'green',
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle: 'miter',
+				pointBorderColor: 'white',
+				pointBackgroundColor: 'white',
+				pointBorderWidth: 10,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+				pointHoverBorderColor: 'rgba(220, 220, 220,1)',
+				pointHoverBorderWidth: 2,
+				pointRadius: 1,
+				pointHitRadius: 10,
+				data: chartData
+			}
+		]
+	};
+</script>
+
+<Line {data} options={{ responsive: true }} />
--- src/lib/components/pages/project/ProjectMain.svelte
+++ src/lib/components/pages/project/ProjectMain.svelte
@@ -1,1 +1,11 @@
-<div>maaain</div>
+<script>
+	import LineChart from '$components/charts/LineChart.svelte';
+</script>
+
+<div>
+	<LineChart
+		title="Funding"
+		chartData={[10, 20, 35, 76, 140, 200, 310]}
+		labels={['January', 'February', 'March', 'April', 'May', 'June', 'July']}
+	/>
+</div>
