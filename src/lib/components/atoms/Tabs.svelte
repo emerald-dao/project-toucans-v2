9728a863d0f5c2d179ea04e9441185a8557a471c
mateoroldos <mateoroldos19@gmail.com>
2022-10-14 18:46:05 -0300

    feat: Tab components basic structure

---
+++ src/lib/components/atoms/Tabs.svelte
@@ -0,0 +1,30 @@
+<script type="ts">
+	import { Button } from '@emerald-dao/component-library';
+	import { Row } from '@mateoroldos/svelte.bones';
+	import type { SvelteComponent } from 'svelte';
+
+	interface TabObject {
+		name: string;
+		component: typeof SvelteComponent;
+	}
+
+	export let tabs: TabObject[];
+	let activeTab = 0;
+
+	const setActiveTab = (index: number) => {
+		activeTab = index;
+	};
+</script>
+
+<Row justify="flex-start">
+	{#each tabs as tab, i}
+		<Button
+			type="transparent"
+			color={i === activeTab ? 'primary' : 'neutral'}
+			on:click={() => setActiveTab(i)}>{tab.name}</Button
+		>
+	{/each}
+</Row>
+<div>
+	<svelte:component this={tabs[activeTab].component} />
+</div>
--- src/lib/components/atoms/index.ts
+++ src/lib/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Card } from './Card.svelte';
 export { default as LeadingProjectCard } from './LeadingProjectCard.svelte';
 export { default as Ball } from './Ball.svelte';
 export { default as Stat } from './Stat.svelte';
+export { default as Tabs } from './Tabs.svelte';
