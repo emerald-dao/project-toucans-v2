ec96909eb01e1336f46ab073a2fc4c6fd413113d
mateoroldos <mateoroldos19@gmail.com>
2022-10-25 09:19:30 -0300

    feat: Transition effects to fund modal steps

--- src/routes/discover/[project]/components/funding-steps/Fund.svelte
+++ src/routes/discover/[project]/components/funding-steps/Fund.svelte
@@ -8,6 +8,7 @@
 	import fundingSuite from '$lib/validations/fundingSuite';
 	import { Currencies } from '$lib/types/currencies.enum';
 	import { RadioButtons } from '$atoms';
+	import { fade } from 'svelte/transition';
 
 	const handleChange = (input: Event) => {
 		res = fundingSuite($fundData, input.target.name);
@@ -34,51 +35,53 @@
 	};
 </script>
 
-<Column gap="small" align="flex-start">
-	<h4>Fund Emerald DAO</h4>
-	<form id="fund-form" on:submit|preventDefault={fundActiveStep.increment} autocomplete="off">
-		<label for="amount">Amount to Found</label>
-		<Column gap={0.2} align="flex-start">
-			<RadioButtons data={currenciesRadioButtonsData} />
-			<InputWrapper
-				name="amount"
-				iconUrl={$fundData.currency === Currencies.FLOW ? '/flow-logo.png' : '/fusd-logo.png'}
-				{res}
-			>
-				<input
-					type="number"
+<div in:fade={{ duration: 200 }}>
+	<Column gap="small" align="flex-start">
+		<h4>Fund Emerald DAO</h4>
+		<form id="fund-form" on:submit|preventDefault={fundActiveStep.increment} autocomplete="off">
+			<label for="amount">Amount to Found</label>
+			<Column gap={0.2} align="flex-start">
+				<RadioButtons data={currenciesRadioButtonsData} />
+				<InputWrapper
 					name="amount"
-					placeholder="1000"
-					bind:value={$fundData.amount}
+					iconUrl={$fundData.currency === Currencies.FLOW ? '/flow-logo.png' : '/fusd-logo.png'}
+					{res}
+				>
+					<input
+						type="number"
+						name="amount"
+						placeholder="1000"
+						bind:value={$fundData.amount}
+						on:input={handleChange}
+					/>
+				</InputWrapper>
+			</Column>
+			<InputWrapper name="message" label="Add a special message" {res}>
+				<textarea
+					name="message"
+					placeholder="Write a special message"
+					bind:value={$fundData.specialMessage}
 					on:input={handleChange}
 				/>
 			</InputWrapper>
-		</Column>
-		<InputWrapper name="message" label="Add a special message" {res}>
-			<textarea
-				name="message"
-				placeholder="Write a special message"
-				bind:value={$fundData.specialMessage}
-				on:input={handleChange}
-			/>
-		</InputWrapper>
 
-		<label for="recieve">You will recieve</label>
-		<Row gap={0.1}>
-			<span class="token-name">${'EMLD'}</span>
-			<input
-				name="recieve"
-				type="text"
-				value={($fundData.amount * $fundData.issuanceRate).toFixed(2)}
-				readonly
-				id="recieve"
-			/>
-		</Row>
-	</form>
-	<Button form="fund-form" size="full-width" state={res.isValid() ? 'active' : 'disabled'}
-		><Icon icon="tabler:pig-money" />Fund</Button
-	>
-</Column>
+			<label for="recieve">You will recieve</label>
+			<Row gap={0.1}>
+				<span class="token-name">${'EMLD'}</span>
+				<input
+					name="recieve"
+					type="text"
+					value={($fundData.amount * $fundData.issuanceRate).toFixed(2)}
+					readonly
+					id="recieve"
+				/>
+			</Row>
+		</form>
+		<Button form="fund-form" size="full-width" state={res.isValid() ? 'active' : 'disabled'}
+			><Icon icon="tabler:pig-money" />Fund</Button
+		>
+	</Column>
+</div>
 
 <style type="scss">
 	form {
--- src/routes/discover/[project]/components/funding-steps/Thanks.svelte
+++ src/routes/discover/[project]/components/funding-steps/Thanks.svelte
@@ -3,35 +3,38 @@
 	import { fundData } from '$stores/fund/FundDataStore';
 	import { Button } from '@emerald-dao/component-library';
 	import { Column, Row } from '@mateoroldos/svelte.bones';
+	import { fade } from 'svelte/transition';
 </script>
 
-<Column>
-	<h4>Thank You!</h4>
-	<p>
-		{`You funded ${$fundData.daoName} with $${$fundData.currency} ${$fundData.amount} and got $${
-			$fundData.tokenName
-		} ${($fundData.amount * $fundData.issuanceRate).toFixed(2)}`}
-	</p>
-	<Column gap={1}>
-		<span>Share it</span>
-		<Row gap={0.8}>
-			<Button
-				href={`https://twitter.com/intent/tweet?text=I%20just%20funded%20${
-					$fundData.daoName
-				}%20DAO%20with%20$${$fundData.currency}%20${$fundData.amount}%20and%20got%20$${
-					$fundData.tokenName
-				}%20${($fundData.amount * $fundData.issuanceRate).toFixed(2)}
+<div in:fade={{ duration: 200 }}>
+	<Column>
+		<h4>Thank You!</h4>
+		<p>
+			{`You funded ${$fundData.daoName} with $${$fundData.currency} ${$fundData.amount} and got $${
+				$fundData.tokenName
+			} ${($fundData.amount * $fundData.issuanceRate).toFixed(2)}`}
+		</p>
+		<Column gap={1}>
+			<span>Share it</span>
+			<Row gap={0.8}>
+				<Button
+					href={`https://twitter.com/intent/tweet?text=I%20just%20funded%20${
+						$fundData.daoName
+					}%20DAO%20with%20$${$fundData.currency}%20${$fundData.amount}%20and%20got%20$${
+						$fundData.tokenName
+					}%20${($fundData.amount * $fundData.issuanceRate).toFixed(2)}
 				`}
-				target="_blank"
-				type="ghost"
-				size="x-small"><Icon icon="tabler:brand-twitter" />Twitter</Button
-			>
-			<Button href="https://discord.com/" target="_blank" type="ghost" size="x-small"
-				><Icon icon="tabler:brand-discord" />Discord</Button
-			>
-		</Row>
+					target="_blank"
+					type="ghost"
+					size="x-small"><Icon icon="tabler:brand-twitter" />Twitter</Button
+				>
+				<Button href="https://discord.com/" target="_blank" type="ghost" size="x-small"
+					><Icon icon="tabler:brand-discord" />Discord</Button
+				>
+			</Row>
+		</Column>
 	</Column>
-</Column>
+</div>
 
 <style type="scss">
 	span {
