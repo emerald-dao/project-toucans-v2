28c153f9c874e99a4cc7e785e9f6e9e837b6c81e
mateoroldos <mateoroldos19@gmail.com>
2022-10-31 17:12:34 -0300

    feat: Add logo to ReveiwAndDeploy step

--- src/routes/dao-generator/components/generator-steps/ReviewAndDeploy.svelte
+++ src/routes/dao-generator/components/generator-steps/ReviewAndDeploy.svelte
@@ -5,13 +5,36 @@
 	import { Column, Row } from '@mateoroldos/svelte.bones';
 	import { TokenTypes } from '$lib/types/token-types.enum';
 	import RecapCard from './atoms/RecapCard.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		if ($daoData.daoDetails.logo) {
+			displayLogo($daoData.daoDetails.logo[0]);
+		}
+	});
+
+	const displayLogo = (file: File) => {
+		let reader = new FileReader();
+		reader.readAsDataURL(file); // base 64 format
+
+		reader.onload = () => {
+			logoElement.src = `${reader.result}`;
+		};
+	};
+
+	let logoElement: HTMLImageElement;
 </script>
 
 <Column gap="small">
 	<RecapCard title="DAO Details" stepNumber={0}>
-		<Row gap="small">
-			<RecapElement title="Dao Name" data={$daoData.daoDetails.name} />
-			<RecapElement title="Token Name" data={$daoData.daoDetails.tokenName} />
+		<Row gap="small" align="flex-start">
+			{#if $daoData.daoDetails.logo}
+				<img bind:this={logoElement} class="logo" alt="Dao logo" />
+			{/if}
+			<Column gap={1} align="flex-start">
+				<RecapElement title="Dao Name" data={$daoData.daoDetails.name} />
+				<RecapElement title="Token Name" data={$daoData.daoDetails.tokenName} />
+			</Column>
 		</Row>
 		<RecapElement title="Description" data={$daoData.daoDetails.description} />
 		<RecapElement title="Website" data={$daoData.daoDetails.website} />
@@ -37,3 +60,13 @@
 	</RecapCard>
 </Column>
 <StepButtons />
+
+<style type="scss">
+	img {
+		height: 120px;
+		aspect-ratio: 1 / 1;
+		border-radius: 0.3rem;
+		object-fit: contain;
+		border: 1px var(--clr-neutral-400) solid;
+	}
+</style>
