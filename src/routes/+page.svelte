b3755365d6e116a081f433135f1db89a57cbf792
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2022-12-26 17:40:14 -0300

    Home page cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,120 +1,15 @@
 <script type="ts">
-	import { Column, Row, AdaptableGrid } from '@mateoroldos/svelte.bones';
-	import { Button, Tagline } from '@emerald-dao/component-library';
-	import { LeadingProjectCard } from '$components/cards/index';
-	import type { LeadingDao } from '$lib/types/leading-dao.interface';
+	import { Column } from '@mateoroldos/svelte.bones';
 	import CreateYourToken from './components/sections/CreateYourToken.svelte';
-
-	let toucansFeatures = [
-		{
-			title: 'Investment',
-			description: 'Start and Investment Club. Pool funds and vote on decisions',
-			icon: '/flow-logo.png'
-		},
-		{
-			title: 'Collaboration',
-			description: 'Fund your project and work towards a common collective goal',
-			icon: '/flow-logo.png'
-		},
-		{
-			title: 'Grant',
-			description: 'Support a cause. Collectively decide which people & projects to fund',
-			icon: '/flow-logo.png'
-		}
-	];
-
-	let testDaos: LeadingDao[] = [
-		{
-			logoUrl: '/toucans-logo.svg',
-			name: 'Toucans DAO',
-			totalInvested: 100,
-			currency: 'FLOW',
-			numberOfPayments: 20,
-			variationPercentage: 8.2
-		},
-		{
-			logoUrl: '/toucans-logo.svg',
-			name: 'Toucans DAO',
-			totalInvested: 100,
-			currency: 'FLOW',
-			numberOfPayments: 20,
-			variationPercentage: 8.2
-		},
-		{
-			logoUrl: '/toucans-logo.svg',
-			name: 'Toucans DAO',
-			totalInvested: 100,
-			currency: 'FLOW',
-			numberOfPayments: 20,
-			variationPercentage: 8.2
-		},
-		{
-			logoUrl: '/toucans-logo.svg',
-			name: 'Toucans DAO',
-			totalInvested: 100,
-			currency: 'FLOW',
-			numberOfPayments: 20,
-			variationPercentage: -8.2
-		}
-	];
+	import Hero from './components/sections/Hero.svelte';
+	import ToucansFeatures from './components/sections/ToucansFeatures.svelte';
+	import LeadingProjects from './components/sections/LeadingProjects.svelte';
 </script>
 
-<div class="section">
-	<div class="container-small">
-		<Column>
-			<h1>Create a token.<br /> Launch a DAO</h1>
-			<p>
-				Toucans is the first fungible token creation and management platform on the Flow blockchain
-				that makes it easy for internet communities unlock the power of human-centric coordination.
-			</p>
-			<Row gap="small">
-				<Button>Launch a Token</Button>
-				<Button type="ghost">Browse Projects</Button>
-			</Row>
-		</Column>
-	</div>
-</div>
-
-<div class="section">
-	<div class="container">
-		<Column>
-			<Tagline>Why Start a DAO?</Tagline>
-			<h2>Tokens for any Community</h2>
-			<AdaptableGrid minWidth="small">
-				{#each toucansFeatures as feature}
-					<div class="card-primary">
-						<Column justify="center" gap="small">
-							<h3>{feature.title}</h3>
-							<p>{feature.description}</p>
-						</Column>
-					</div>
-				{/each}
-			</AdaptableGrid>
-		</Column>
-	</div>
-</div>
-
-<div class="section">
-	<div class="container">
-		<Column>
-			<Tagline>Explore</Tagline>
-			<h2>Leading Projects</h2>
-			<AdaptableGrid gap="small">
-				{#each testDaos as testDao, i}
-					<LeadingProjectCard daoData={testDao} number={i + 1} />
-				{/each}
-			</AdaptableGrid>
-		</Column>
-	</div>
-</div>
-
-<div class="section">
-	<div class="container">
-		<Column>
-			<CreateYourToken />
-		</Column>
-	</div>
-</div>
+<Hero />
+<ToucansFeatures />
+<LeadingProjects />
+<CreateYourToken />
 
 <div class="section">
 	<div class="container">
@@ -131,14 +26,3 @@
 		</Column>
 	</div>
 </div>
-
-<style type="scss">
-	h1,
-	p {
-		text-align: center;
-	}
-
-	h3 {
-		font-size: var(--fs-500);
-	}
-</style>
