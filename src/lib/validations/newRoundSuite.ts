e4506a3ab6367095e466a5b0e940913bce3eda54
mateoroldos <mateoroldos19@gmail.com>
2022-12-26 11:57:31 -0300

    feat: GeneralInfo rounds form imporvements

--- src/lib/validations/newRoundSuite.ts
+++ src/lib/validations/newRoundSuite.ts
@@ -1,8 +1,23 @@
-import { create, enforce, test } from 'vest';
+import { create, enforce, test, only } from 'vest';
 
-const newRoundSuite = create((data = {}) => {
+const newRoundSuite = create((data = {}, currentField) => {
+	only(currentField);
+
+	console.log(currentField);
+
 	test('fundingGoal', 'Funding goal must be greater tha 0', () => {
 		enforce(data.fundingGoal).greaterThan(0);
+	});
+
+	test('startDate', 'Start date must be equal or greater than today', () => {
+		console.log('aa');
+
+		enforce(new Date(data.startDate).getTime()).greaterThanOrEquals(new Date().getTime());
+	});
+
+	test('endDate', 'End date must be greater than start date', () => {
+		console.log('bb');
+		enforce(new Date(data.endDate).getTime()).greaterThan(new Date(data.startDate).getTime());
 	});
 });
 
--- src/routes/admin/rounds/components/steps/GeneralInfo.svelte
+++ src/routes/admin/rounds/components/steps/GeneralInfo.svelte
@@ -1,46 +1,99 @@
 <script type="ts">
+	import { onMount } from 'svelte';
 	import newRoundSuite from '$lib/validations/newRoundSuite';
 	import { InputWrapper } from '@emerald-dao/component-library';
 
+	let now = new Date();
+	let nowString = now.toISOString().split('T')[0];
+	let oneMonthForwardSting = new Date(new Date().setMonth(new Date().getMonth() + 1))
+		.toISOString()
+		.split('T')[0];
+
 	let formData: FormData = {
+		startDate: nowString,
+		endDate: oneMonthForwardSting,
 		fundingGoal: 0
 	};
 
 	interface FormData {
+		startDate: string;
+		endDate: string;
 		fundingGoal: number;
 	}
 
-	const handleChange = () => {
-		res = newRoundSuite(formData);
-		console.log(res);
+	const handleChange = (input: Event) => {
+		const target = input.target as HTMLInputElement;
+
+		res = newRoundSuite(formData, target.name);
 	};
 
+	onMount(() => {
+		startDateInput.min = nowString;
+	});
+
+	let startDateInput: HTMLInputElement;
+	let endDateInput: HTMLInputElement;
+
 	let res = newRoundSuite.get();
+
+	$: if (endDateInput) endDateInput.min = formData.startDate;
+	$: if (new Date(formData.startDate) > new Date(formData.endDate))
+		formData.endDate = new Date(
+			new Date(formData.startDate).setMonth(new Date(formData.startDate).getMonth() + 1)
+		)
+			.toISOString()
+			.split('T')[0];
 </script>
 
 <div class="column-4">
 	<span>General Info</span>
-	<form class="column-4">
+	<form class="column-4" autocomplete="off">
 		<div class="row-6">
 			<div>
-				<label for="start-date">Start date</label>
-				<input type="date" id="start-date" />
+				<InputWrapper
+					name="startDate"
+					errors={res.getErrors('startDate')}
+					isValid={res.isValid('startDate')}
+					label="Start date"
+				>
+					<input
+						type="date"
+						name="startDate"
+						id="startDate"
+						bind:this={startDateInput}
+						bind:value={formData.startDate}
+						on:input={handleChange}
+					/>
+				</InputWrapper>
 			</div>
 			<div>
-				<label for="end-date">End date</label>
-				<input type="date" id="end-date" />
+				<InputWrapper
+					name="endDate"
+					errors={res.getErrors('endDate')}
+					isValid={res.isValid('endDate')}
+					label="End date"
+				>
+					<input
+						type="date"
+						name="endDate"
+						id="endDate"
+						bind:this={endDateInput}
+						bind:value={formData.endDate}
+						on:input={handleChange}
+					/>
+				</InputWrapper>
 			</div>
 		</div>
 		<div>
 			<InputWrapper
-				name="funding-goal"
+				name="fundingGoal"
 				errors={res.getErrors('fundingGoal')}
 				isValid={res.isValid('fundingGoal')}
 				label="Funding goal"
 			>
 				<input
 					type="number"
-					id="funding-goal"
+					name="fundingGoal"
 					bind:value={formData.fundingGoal}
 					on:input={handleChange}
 				/>
