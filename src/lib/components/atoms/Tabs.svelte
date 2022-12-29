b0fcd59699a98f5c8ffcb742b9cd21a3021037e8
mateoroldos <mateoroldos19@gmail.com>
2022-12-29 11:42:38 -0300

    feat: Tabs improvement

--- src/lib/components/atoms/Tabs.svelte
+++ src/lib/components/atoms/Tabs.svelte
@@ -1,6 +1,4 @@
 <script type="ts">
-	import { Button } from '@emerald-dao/component-library';
-	import { Row } from '@mateoroldos/svelte.bones';
 	import type { SvelteComponent } from 'svelte';
 
 	interface TabObject {
@@ -17,13 +15,13 @@
 </script>
 
 <div class="main-wrapper">
-	<Row justify="flex-start">
+	<div class="top-wrapper">
 		{#each tabs as tab, i}
 			<div class:active={activeTab === i} class="name" on:click={() => setActiveTab(i)} on:keydown>
 				{tab.name}
 			</div>
 		{/each}
-	</Row>
+	</div>
 	<div class="tab-wrapper">
 		<svelte:component this={tabs[activeTab].component} />
 	</div>
@@ -37,14 +35,27 @@
 		height: 100%;
 		min-height: 1px;
 
-		.name {
-			cursor: pointer;
-			padding-bottom: var(--space-2);
-		}
+		.top-wrapper {
+			display: flex;
+			flex-direction: row;
+			gap: var(--space-10);
+			border-bottom: 1px var(--clr-border-primary) solid;
 
-		.active {
-			color: var(--clr-heading-main);
-			border-bottom: 1px solid var(--clr-heading-main);
+			.name {
+				cursor: pointer;
+				padding-bottom: var(--space-2);
+				font-size: var(--font-size-1);
+				transition: 0.1s;
+			}
+
+			.name:hover {
+				color: var(--clr-heading-main);
+			}
+
+			.active {
+				color: var(--clr-heading-main);
+				border-bottom: 2px solid var(--clr-heading-main);
+			}
 		}
 
 		.tab-wrapper {
