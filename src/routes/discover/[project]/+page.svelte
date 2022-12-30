33b116ef69cd7046ad5a13db0762a43d24dcdc68
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2022-12-30 16:48:39 -0300

    Project View Sidebar

--- src/routes/discover/[project]/+page.svelte
+++ src/routes/discover/[project]/+page.svelte
@@ -4,6 +4,10 @@
 	import { ProjectSidebarSection } from './components';
 	import FinancialProject from './components/sections/FinancialProject.svelte';
 	import { financialDaoData } from '$lib/mock/financialDao';
+	import SeeMoreSidebar from './components/atoms/SeeMoreSidebar.svelte';
+	import Icon from '@iconify/svelte';
+
+	let seeMore = false;
 
 	let daoData: FinancialDao | CommunityDao = financialDaoData;
 </script>
@@ -19,6 +23,14 @@
 			{/if}
 		</div>
 	</div>
+	<div class="button" on:click={() => (seeMore = !seeMore)} on:keydown>
+		<Icon icon="tabler:arrow-left" color="#ff66c4" width="19.5" />
+		<p class="xsmall w-medium">More</p>
+	</div>
+
+	{#if seeMore}
+		<SeeMoreSidebar on:closeModal={() => (seeMore = !seeMore)} />
+	{/if}
 </section>
 
 <style type="scss">
@@ -32,6 +44,21 @@
 			display: grid;
 			grid-template-columns: 1.3fr 2fr;
 			gap: 4rem;
+		}
+	}
+	.button {
+		position: fixed;
+		right: 0;
+		top: 20vh;
+		display: flex;
+		align-items: center;
+		padding: var(--space-2) var(--space-4);
+		border: solid 1px var(--clr-tertiary-main);
+		border-radius: var(--radius-1) 0px 0px var(--radius-1);
+		cursor: pointer;
+
+		p {
+			color: var(--clr-tertiary-main);
 		}
 	}
 </style>
