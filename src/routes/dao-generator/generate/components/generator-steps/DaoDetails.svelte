3978c9dc14b22fce44de43adc5ae9caca1d15182
jacob-tucker <jacobtucker818@gmail.com>
2023-01-09 17:01:47 -0500

    data

--- src/routes/dao-generator/generate/components/generator-steps/DaoDetails.svelte
+++ src/routes/dao-generator/generate/components/generator-steps/DaoDetails.svelte
@@ -1,6 +1,9 @@
 <script type="ts">
+	import { page } from '$app/stores';
 	import DaoDetailsForm from '$lib/components/forms/DaoDetailsForm.svelte';
 	import StepButtons from './atoms/StepButtons.svelte';
+
+	console.log($page.data);
 </script>
 
 <DaoDetailsForm />
