5cc72bf143a472cb78becce06956b5610d01f72f
jacob-tucker <jacobtucker818@gmail.com>
2023-01-19 16:25:01 -0500

    token symbol

--- src/routes/dao-generator/generate/+page.js
+++ src/routes/dao-generator/generate/+page.js
@@ -1,7 +1,7 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load({ params }) {
-  const data = await supabase.from('projects').select('name, contract_name');
+  const data = await supabase.from('projects').select('name, token_symbol');
   return {
     data
   };
