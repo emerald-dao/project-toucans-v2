a43429ca8b200cc998cb67d83e18e5651fd8731d
mateoroldos <mateoroldos19@gmail.com>
2023-01-31 15:17:53 -0300

    feat: Add typescript to server endpoint

---
+++ src/routes/api/add/+server.ts
@@ -0,0 +1,37 @@
+import { json } from '@sveltejs/kit';
+import { createClient } from '@supabase/supabase-js';
+import { env as PrivateEnv } from '$env/dynamic/private';
+import { env as PublicEnv } from '$env/dynamic/public';
+import { verifyAccountOwnership } from '$flow/utils.js';
+import type { RequestHandler } from './$types';
+
+const supabase = createClient(PublicEnv.PUBLIC_SUPABASE_URL, PrivateEnv.SUPABASE_SERVICE_KEY);
+
+export async function POST({ request }: { request: RequestHandler }) {
+	const data = await request.json();
+
+	// Make sure a valid user was passed in
+	const verifyAccount = await verifyAccountOwnership(data.user);
+	if (!verifyAccount) {
+		return json({});
+	}
+
+	const tokenName = data.daoDetails.tokenName;
+	const name = data.daoDetails.name;
+
+	const { error } = await supabase.from('projects').insert({
+		name,
+		token_symbol: tokenName,
+		contract_name: data.daoDetails.contractName,
+		contract_address: data.user.addr,
+		description: data.daoDetails.description,
+		website: data.daoDetails.website,
+		twitter: data.daoDetails.twitter,
+		discord: data.daoDetails.discord,
+		logo: data.logo,
+		owner: data.user.addr,
+		type: data.tokenomics.tokenType
+	});
+
+	return json(error);
+}
