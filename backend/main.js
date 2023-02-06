9a523a6c1a7c5a3c8fe283525b0e76ab38429c41
Jacob <jacobtucker818@gmail.com>
2023-02-06 11:53:41 -0500

    fix error

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 
 const express = require('express');
-const { supabase } = require('./utils');
+// const { supabase } = require('./utils');
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
