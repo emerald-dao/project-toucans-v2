c53749f9b65cfcb733f82da01778c4de54d21f19
Jacob <jacobtucker818@gmail.com>
2023-02-06 11:55:47 -0500

    fix

--- backend/main.js
+++ backend/main.js
@@ -6,4 +6,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
