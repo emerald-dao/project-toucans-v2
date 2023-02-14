31eeab17a719750eff6f5751c9c711c6ba46d58c
Jacob <jacobtucker818@gmail.com>
2023-02-14 15:56:41 -0500

    move flow config to env

--- frontend/src/flow/config.js
+++ frontend/src/flow/config.js
@@ -1,5 +1,6 @@
 import { config } from '@onflow/fcl';
 import dappInfo from '$lib/config/config';
+import { env } from '$env/dynamic/public';
 
 const resolver = async () => {
 	// const response = await fetch('/api/generate');
@@ -10,18 +11,17 @@ const resolver = async () => {
 	}
 }
 
+export const network = env.PUBLIC_FLOW_NETWORK;
+
 // EMULATOR
-export const network = 'emulator';
-const accessNode = 'http://127.0.0.1:8888';
-const discoveryWallet = 'http://localhost:8701/fcl/authn';
+// const accessNode = 'http://127.0.0.1:8888';
+// const discoveryWallet = 'http://localhost:8701/fcl/authn';
 
 // TESTNET
-// export const network = 'testnet';
 // const accessNode = 'https://rest-testnet.onflow.org';
 // const discoveryWallet = 'https://fcl-discovery.onflow.org/testnet/authn';
 
 // MAINNET
-// export const network = 'mainnet';
 // const accessNode = 'https://rest-mainnet.onflow.org';
 // const discoveryWallet = 'https://fcl-discovery.onflow.org/authn';
 
@@ -30,5 +30,5 @@ config()
 	.put('app.detail.icon', dappInfo.icon)
 	.put("fcl.accountProof.resolver", resolver)
 	.put('flow.network', network)
-	.put('accessNode.api', accessNode)
-	.put('discovery.wallet', discoveryWallet)
+	.put('accessNode.api', env.PUBLIC_FLOW_ACCESS_NODE)
+	.put('discovery.wallet', env.PUBLIC_FLOW_DISCOVERY)
