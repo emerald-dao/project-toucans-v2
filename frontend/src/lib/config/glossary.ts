86f72731b62a250e0c06806277ed238e54f5c467
mateoroldos <mateoroldos19@gmail.com>
2023-04-19 12:11:56 -0300

    feat: Glossary structure

---
+++ frontend/src/lib/config/glossary.ts
@@ -0,0 +1,7 @@
+const GLOSSARY = {
+	threshold: 'Amount of signatures required to execute a transaction',
+	maxSupply: 'Maximum amount of tokens that can be minted',
+	minting: 'When this feature is enabled, the DAO admin can mint new tokens'
+};
+
+export default GLOSSARY;
---
+++ frontend/src/routes/glossary/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import GLOSSARY from '$lib/config/glossary';
+
+	function formatTerm(inputString: string): string {
+		const words = inputString.split(/(?=[A-Z])/);
+
+		words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+
+		for (let i = 1; i < words.length; i++) {
+			words[i] = words[i].charAt(0).toLowerCase() + words[i].slice(1);
+		}
+
+		const outputString = words.join(' ');
+
+		return outputString;
+	}
+</script>
+
+<section class="container-small">
+	<h1>Glossary</h1>
+	{#each Object.entries(GLOSSARY) as [term, definition]}
+		<h2>{formatTerm(term)}</h2>
+		<p>{definition}</p>
+	{/each}
+</section>
+
+<style lang="scss">
+	h1 {
+		margin-bottom: var(--space-12);
+	}
+
+	h2 {
+		font-size: var(--font-size-4);
+		margin-bottom: var(--space-2);
+	}
+
+	p {
+		margin-bottom: var(--space-12);
+	}
+</style>
