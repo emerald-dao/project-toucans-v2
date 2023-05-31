cc0469cf111fd19d97e2801067716c4e842418b7
Jacob Tucker <tsnakejake@aol.com>
2023-05-31 02:17:53 -0500

    feat: pagination to recent activity

--- frontend/src/lib/components/dao-data-blocks/events/EventsList.svelte
+++ frontend/src/lib/components/dao-data-blocks/events/EventsList.svelte
@@ -1,23 +1,40 @@
 <script type="ts">
 	import { getFindNamesBatch } from '$flow/utils';
 	import type { DAOProject } from '$lib/types/dao-project/dao-project.interface';
+	import { Button } from '@emerald-dao/component-library';
 	import EventsListElement from './EventsListElement.svelte';
 	import { getUsersFromEvents } from './functions/getUsersFromEvents';
+	import Icon from '@iconify/svelte';
 
 	export let daoData: DAOProject;
+	let currentPage = 1;
+	const pageSize = 6;
 
-	$: recentActivity = daoData.events
-		? daoData.events
-				.sort((a, b) => (a.timestamp < b.timestamp ? 1 : a.timestamp > b.timestamp ? -1 : 0))
-				.slice(0, 6)
+	const nextPage = () => {
+		currentPage += 1;
+	};
+
+	const prevPage = () => {
+		currentPage -= 1;
+	};
+
+	$: allActivity = daoData.events
+		? daoData.events.sort((a, b) =>
+				a.timestamp < b.timestamp ? 1 : a.timestamp > b.timestamp ? -1 : 0
+		  )
 		: [];
-	$: addressList = getUsersFromEvents(recentActivity);
+	$: pageStart = (currentPage - 1) * pageSize;
+	$: pageEnd = pageStart + pageSize;
+	$: pagesNumbers = Array.from(Array(Math.ceil(daoData.events.length / pageSize)).keys());
+	$: currentPageEvents = allActivity.slice(pageStart, pageEnd);
+
+	$: addressList = getUsersFromEvents(currentPageEvents);
 </script>
 
 <div class="align-start">
-	{#if recentActivity.length > 0}
+	{#if currentPageEvents.length > 0}
 		{#await getFindNamesBatch(addressList) then findNames}
-			{#each recentActivity as event, i}
+			{#each currentPageEvents as event, i}
 				<div class="activity-wrapper">
 					<EventsListElement {event} {i} {daoData} {findNames} />
 				</div>
@@ -26,6 +43,26 @@
 	{:else}
 		<div class="no-events-wrapper">
 			<span class="small"><em>This DAO has no events yet</em></span>
+		</div>
+	{/if}
+	{#if pagesNumbers.length > 1}
+		<div class="pagination row-4">
+			<Button
+				on:click={prevPage}
+				state={currentPage === 1 ? 'disabled' : 'active'}
+				type="transparent"
+				color="neutral"
+			>
+				<Icon icon="tabler:arrow-left" />
+			</Button>
+			<Button
+				on:click={nextPage}
+				state={pageEnd >= allActivity.length ? 'disabled' : 'active'}
+				type="transparent"
+				color="neutral"
+			>
+				<Icon icon="tabler:arrow-right" />
+			</Button>
 		</div>
 	{/if}
 </div>
