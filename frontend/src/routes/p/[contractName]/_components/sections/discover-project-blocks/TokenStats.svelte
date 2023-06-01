03f21a562ea51ec2544a69951d4eb9734fd6900e
Jacob Tucker <tsnakejake@aol.com>
2023-05-31 19:37:55 -0500

    add more token stats

--- frontend/src/routes/p/[contractName]/_components/sections/discover-project-blocks/TokenStats.svelte
+++ frontend/src/routes/p/[contractName]/_components/sections/discover-project-blocks/TokenStats.svelte
@@ -4,12 +4,40 @@
 	import type { DAOProject } from '$lib/types/dao-project/dao-project.interface';
 	import { user } from '$stores/flow/FlowStore';
 	import { Button } from '@emerald-dao/component-library';
-	import { setUpVaultExecution } from '$flow/actions';
+	import { getProjectInfo, setUpVaultExecution } from '$flow/actions';
+	import { formatFix } from '$flow/utils';
 
 	export let daoData: DAOProject;
 	async function setUpVault() {
 		await setUpVaultExecution(daoData.generalInfo.project_id, daoData.generalInfo.contract_address);
 		daoData.vaultSetup = true;
+	}
+
+	interface TokenData {
+		numHolders: number;
+		averageHolding: number;
+		numFunders: number;
+		averageFunding: number;
+	}
+
+	async function calculateTokenData(): Promise<TokenData> {
+		return await new Promise((resolve, reject) => {
+			let numHolders, totalHolding, numFunders, totalFunding;
+			numHolders = totalHolding = numFunders = totalFunding = 0;
+
+			for (const holder in daoData.onChainData.balances) {
+				if (holder === daoData.generalInfo.owner) continue;
+				numHolders++;
+				totalHolding += Number(daoData.onChainData.balances[holder]);
+			}
+			let averageHolding = totalHolding / numHolders;
+			for (const funder in daoData.onChainData.funders) {
+				numFunders++;
+				totalFunding += Number(daoData.onChainData.funders[funder]);
+			}
+			let averageFunding = totalFunding / numFunders;
+			resolve({ numHolders, averageHolding, numFunders, averageFunding });
+		});
 	}
 </script>
 
@@ -58,6 +86,24 @@
 			isCurrency
 		/>
 	</div>
+	{#await calculateTokenData() then tokenData}
+		<div class="secondary-wrapper-small">
+			<DataCard title="Unique Holders" data={tokenData.numHolders} />
+			<DataCard
+				title="Average Holding"
+				data={tokenData.averageHolding}
+				isCurrency
+				currencyName={daoData.generalInfo.token_symbol}
+			/>
+			<DataCard title="Unique Funders" data={tokenData.numFunders} />
+			<DataCard
+				title="Average Funding"
+				data={tokenData.averageFunding}
+				isCurrency
+				currencyName={daoData.onChainData.paymentCurrency}
+			/>
+		</div>
+	{/await}
 </div>
 
 <style lang="scss">
@@ -72,6 +118,17 @@
 			flex-direction: column;
 
 			@include mq('small') {
+				flex-direction: row;
+			}
+		}
+
+		.secondary-wrapper-small {
+			display: grid;
+			gap: var(--space-5);
+			grid-template-columns: 1fr 1fr;
+
+			@include mq('small') {
+				display: flex;
 				flex-direction: row;
 			}
 		}
