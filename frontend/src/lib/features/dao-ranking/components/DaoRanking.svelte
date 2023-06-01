52e3aeee978b10c471dfcc9d12f76232b79960c1
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-06-01 16:28:02 -0300

    DaoRanking assembly

---
+++ frontend/src/lib/features/dao-ranking/components/DaoRanking.svelte
@@ -0,0 +1,42 @@
+<script>
+	import RankingTable from './atoms/RankingTable.svelte';
+
+	let projectsData = [
+		{
+			number: 1,
+			name: 'Dao1',
+			price: 300,
+			hour: 2,
+			day: 1,
+			week: 0,
+			circulatingSupply: 120,
+			chart: {
+				title: 'Prueba',
+				numbers: [2, 4, 5, 7, 8, 9, 10],
+				labels: ['', '', '', '', '', '', '']
+			}
+		},
+		{
+			number: 2,
+			name: 'Dao2',
+			price: 4500,
+			hour: 6,
+			day: 7,
+			week: 5,
+			circulatingSupply: 300,
+			chart: {
+				title: 'Prueba',
+				numbers: [2, 4, 5, 7, 3, 9, 6],
+				labels: ['', '', '', '', '', '', '']
+			}
+		}
+	];
+</script>
+
+<section class="container">
+	<div class="card">
+		<div class="dao-wrapper">
+			<RankingTable {projectsData} />
+		</div>
+	</div>
+</section>
---
+++ frontend/src/lib/features/dao-ranking/components/atoms/RankingTable.svelte
@@ -0,0 +1,65 @@
+<script type="ts">
+	import LineChart from '$components/charts/LineChart.svelte';
+
+	export let projectsData;
+
+	console.log(projectsData.index);
+</script>
+
+<div class="table-wrap">
+	<table class="table table-bordered table-dark table-hover">
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Name</th>
+				<th>Price</th>
+				<th>1 hour</th>
+				<th>1 day</th>
+				<th>1 week</th>
+				<th>Circulating Supply</th>
+				<th>Last 7 days</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each projectsData as project}
+				<tr>
+					<th scope="row">{project.number}</th>
+					<td>{project.name}</td>
+					<td>{project.price}</td>
+					<td>{project.hour}</td>
+					<td>{project.day}</td>
+					<td>{project.week}</td>
+					<td>{project.circulatingSupply}</td>
+					<td>
+						<LineChart
+							title={project.chart.title}
+							chartData={project.chart.numbers}
+							labels={project.chart.labels}
+						/>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
+
+<style type="scss">
+	.table {
+		width: 100%;
+	}
+	.table thead th {
+		border: none;
+		padding: 0 var(--space-3) var(--space-4) var(--space-3);
+		color: var(--clr-heading-main);
+	}
+	.table tbody tr {
+		margin-bottom: var(--space-4);
+	}
+	.table tbody th,
+	.table tbody td {
+		width: calc(100% / 8) !important;
+		border: none;
+		padding: 0 var(--space-3) var(--space-4) var(--space-3);
+		text-align: center;
+	}
+</style>
