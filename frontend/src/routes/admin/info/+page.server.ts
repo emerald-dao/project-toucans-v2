942c9b383c6b9bf412a24ee9eb8b0064d5f61220
mateoroldos <mateoroldos19@gmail.com>
2023-06-01 11:05:47 -0300

    feat: Edit logo and banner image

--- frontend/src/routes/admin/info/+page.server.ts
+++ frontend/src/routes/admin/info/+page.server.ts
@@ -2,6 +2,7 @@ import { fail } from '@sveltejs/kit';
 import { createClient } from '@supabase/supabase-js';
 import { env as PrivateEnv } from '$env/dynamic/private';
 import { env as PublicEnv } from '$env/dynamic/public';
+import uploadToIPFS from '$lib/utilities/uploadToIpfs';
 
 const supabase = createClient(PublicEnv.PUBLIC_SUPABASE_URL, PrivateEnv.SUPABASE_SERVICE_KEY);
 
@@ -15,6 +16,24 @@ export const actions = {
 		const description = formData.get('description');
 		const long_description = formData.get('longDescription');
 
+		const logo = formData.get('logo') as File;
+		const bannerImage = formData.get('banner') as File;
+
+		let logoIpfsUrl;
+		let bannerLogoIpfsUrl;
+
+		if (logo.type === 'image/png' || logo.type === 'image/jpeg' || logo.type === 'image/jpg') {
+			logoIpfsUrl = await uploadToIPFS(logo);
+		}
+
+		if (
+			bannerImage.type === 'image/png' ||
+			bannerImage.type === 'image/jpeg' ||
+			bannerImage.type === 'image/jpg'
+		) {
+			bannerLogoIpfsUrl = await uploadToIPFS(bannerImage);
+		}
+
 		const { error } = await supabase
 			.from('projects')
 			.update({
@@ -22,7 +41,9 @@ export const actions = {
 				twitter,
 				discord,
 				description,
-				long_description
+				long_description,
+				logo: logoIpfsUrl,
+				banner_image: bannerLogoIpfsUrl
 			})
 			.eq('project_id', formData.get('project_id'));
 
