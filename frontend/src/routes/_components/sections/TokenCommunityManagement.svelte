1704789762064f495c98a7c5b39d419b49edf007
pedroinciarte <pedro.inciarte13@gmail.com>
2023-12-22 16:31:46 -0300

    Add Token Community managment component

---
+++ frontend/src/routes/_components/sections/TokenCommunityManagement.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+</script>
+<section class="section-small">
+    <h1>
+        Token & community managment made <br>incredibly easy
+    </h1>
+    <img
+        src="/tokenAndCommunity.png"
+        alt="tokenAndCommunity"
+    >
+</section>
+
+<style lang="scss">
+    section{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    
+        h1 {
+            text-align: center;
+            margin-bottom: var(--space-6) 0;
+            padding-bottom: 20px;
+            font-size: 2.5rem;
+        }
+
+        @include mq('small'){
+		h1{
+			font-size: 70px;
+			}
+        }
+    }
+</style>
