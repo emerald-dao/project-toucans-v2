6794cbafb46235782202fd7835ead8f560fc8858
pedroinciarte <pedro.inciarte13@gmail.com>
2023-12-22 16:35:52 -0300

    Add Community type component

---
+++ frontend/src/routes/_components/sections/CommunityTypesUnlock.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import CommunityType from "./atoms/CommunityType.svelte";
+
+    let names=["Gaming communities","NFT Collectors", "Meme Coins","Open Source Projects","Educational communities", "Whatever you can imagine!"]
+
+    let icons=["noto:video-game","emojione-v1:octopus","twemoji:see-no-evil-monkey","noto:laptop-computer","noto:graduation-cap","noto:light-bulb"]
+</script>
+
+<div class="section-large">
+    <div class="container-medium">
+        <div class="wrapper">
+            <div class="left-wrapper">
+                <h1>Unlock the potential of any community</h1>
+                <p >Unlock transparent funding and governance on the Flow Blockchain.</p>
+            </div>
+            <div class="right-wrapper">
+                {#each names as name,i }
+                    <CommunityType {name} icon={icons[i]}/>
+                {/each}
+            </div>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+    .section-large {
+        .container-medium {
+            width: min(90%, 72rem);
+            margin-inline: auto;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 40px;
+
+            .wrapper {
+                gap:88px;
+                @include mq('medium') {
+                    grid-template-columns: 1fr 1fr;
+                    display: grid;
+                }
+                align-items: center; 
+            }
+
+            .left-wrapper {
+                max-width: 497px;
+                p {
+                    font-size: 1.5rem;
+                    line-height: 26px;
+                    padding-top: 20px;
+                    padding-bottom: 20px;
+
+                }
+            
+                h1{
+                    font-size: 2.5rem;
+                }
+                @include mq('small'){
+                    h1{
+                        font-size: 70px;
+                    }
+                    
+                    p{
+                        font-size: 26px;
+                        line-height: 31px;
+                    }
+                }
+            }
+
+            .right-wrapper {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 20px;
+                @include mq('medium') {
+                    margin-top: 0;
+                }
+            }
+        }
+    }
+</style>
---
+++ frontend/src/routes/_components/sections/atoms/CommunityType.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+
+    export let name: string
+    export let icon: string
+</script>
+
+<span class="content" >
+        <Icon icon={icon} height="44px" style=" vertical-align:text-bottom; padding-right:10px;"/>
+        {name}
+ </span>
+
+
+<style lang="scss">
+    .content {
+        display: flex;
+        width: fit-content;
+        color: white;
+        padding: 8px;
+        align-items: center;
+        border-style: solid;
+        background-color: #1F1E1E;
+        border-radius: 8px;
+        border-color: #1F1E1E;
+        font-size: 1.0rem;
+        @include mq('small'){
+            font-size: 26px;
+        }
+    }
+</style>
