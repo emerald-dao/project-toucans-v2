7af33d75d014fbe3397ef21ada31fcc593ab6717
mateoroldos <mateoroldos19@gmail.com>
2024-01-20 11:38:15 -0300

    feat: Improve feature cards responsive styles

--- frontend/src/routes/_components/sections/community-toolbox-section/atoms/feature-card/FeatureCardContent.svelte
+++ frontend/src/routes/_components/sections/community-toolbox-section/atoms/feature-card/FeatureCardContent.svelte
@@ -9,8 +9,12 @@
 <style lang="scss">
 	div {
 		width: 100%;
-		padding-inline: var(--space-12);
+		padding-inline: var(--space-8);
 		margin-bottom: -3rem;
 		display: grid;
+
+		@include mq('small') {
+			padding-inline: var(--space-12);
+		}
 	}
 </style>
--- frontend/src/routes/_components/sections/community-toolbox-section/atoms/feature-card/FeatureCardHeader.svelte
+++ frontend/src/routes/_components/sections/community-toolbox-section/atoms/feature-card/FeatureCardHeader.svelte
@@ -4,9 +4,14 @@
 
 <style lang="scss">
 	div {
-		padding-inline: var(--space-12);
-		padding-block: var(--space-12) var(--space-8);
+		padding-inline: var(--space-8);
+		padding-block: var(--space-8) var(--space-6);
 		align-items: flex-start;
 		font-size: var(--font-size-3);
+
+		@include mq('small') {
+			padding-inline: var(--space-12);
+			padding-block: var(--space-12) var(--space-8);
+		}
 	}
 </style>
