c6313064f059e38dae7f91b0d4b5d42ceb7d51d6
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-04-16 11:08:08 -0300

    feat: +page.svelte improvements

--- frontend/src/routes/p/[projectId]/nft-treasury/+page.svelte
+++ frontend/src/routes/p/[projectId]/nft-treasury/+page.svelte
@@ -1,36 +1,50 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import Pagination from '$components/atoms/Pagination.svelte';
 	import NftCard from '$lib/features/nft-treasury/components/nfts-list/atoms/NFTCard.svelte';
+	import type { DAOProject } from '$lib/types/dao-project/dao-project.interface.js';
+	import { getContext } from 'svelte';
 
 	export let data;
+
+	const daoData: DAOProject = getContext('daoData');
+
+	const allNFTs = Object.entries(data.projectNFTTreasury).flatMap(([identifier, nfts]) =>
+		nfts.map((nft) => ({ nft, identifier }))
+	);
+
+	let pageSize = 8;
+	let pageStart: number;
+	let pageEnd: number;
+	$: currentPageNFTs = allNFTs.slice(pageStart, pageEnd);
 </script>
 
-<ul>
+<ul class="row-4">
 	{#each data.onChainData.allowedNFTCollections as collection}
-		<li><a href="/p/{$page.params.projectId}/nft-treasury/{collection}">{collection}</a></li>
+		<li>
+			<a href={`/p/${$page.params.projectId}/nft-treasury/${collection}`}>{collection}</a>
+		</li>
 	{/each}
 </ul>
+<div class="nfts-container">
+	{#each currentPageNFTs as nftObject (nftObject.nft.id)}
+		<NftCard
+			nft={nftObject.nft}
+			clickable={false}
+			isSelected={false}
+			selectedCollection={nftObject.identifier}
+			donatedBy={daoData.generalInfo.nftUuidOwnerMap[nftObject.nft.uuid]}
+		/>
+	{/each}
+</div>
 
-{#each Object.keys(data.projectNFTTreasury) as collection}
-	<h2>{collection}</h2>
-	<div class="nfts-container">
-		{#each data.projectNFTTreasury[collection] as nft (nft.id)}
-			<NftCard
-				{nft}
-				clickable={false}
-				isSelected={false}
-				selectedCollection={data.onChainData.allowedNFTCollections[2]}
-				donatedBy={undefined}
-			/>
-		{/each}
-	</div>
-{/each}
+<Pagination bind:pageStart bind:pageEnd amountOfItems={allNFTs.length} {pageSize} />
 
 <style lang="scss">
 	.nfts-container {
-		display: flex;
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
 		flex-wrap: wrap;
-		gap: 20px;
-		padding: var(--space-8) 0;
+		gap: var(--space-4);
 	}
 </style>
